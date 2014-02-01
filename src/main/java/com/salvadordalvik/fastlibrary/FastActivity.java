db6ecb04152da35155df23887dbbf76d8ffc2279
Matthew Shepard <mattshepard@gmail.com>
2014-02-01 00:52:00 -0700

    Stub FastActivity functionality.

--- src/main/java/com/salvadordalvik/fastlibrary/FastActivity.java
+++ src/main/java/com/salvadordalvik/fastlibrary/FastActivity.java
@@ -1,11 +1,21 @@
 package com.salvadordalvik.fastlibrary;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * FastLib
  * Created by Matthew Shepard on 11/17/13.
  */
 public class FastActivity extends Activity {
+    private final int layoutId;
+    public FastActivity(int layoutId) {
+        this.layoutId = layoutId;
+    }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layoutId);
+    }
 }
