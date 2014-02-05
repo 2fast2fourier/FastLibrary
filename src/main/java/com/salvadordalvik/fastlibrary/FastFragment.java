cbc8dfd9a889164363f5c5a700d2e5ed013293d4
Matthew Shepard <mattshepard@gmail.com>
2014-02-05 01:43:05 -0700

    Add generic container.

--- src/main/java/com/salvadordalvik/fastlibrary/FastFragment.java
+++ src/main/java/com/salvadordalvik/fastlibrary/FastFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
